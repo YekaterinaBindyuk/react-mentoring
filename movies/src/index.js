5a6807320d539ecd4393d756819c551bdda67ab6
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-03-06 09:58:05 +0600

    task1 moved to movies

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/App';
+
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      React.createElement('span', {}, 'Greetings from')
+    )
+  }
+}
+
+class Name extends React.PureComponent {
+  render() {
+    const {firstName, lastName} = this.props;
+    return (
+      <span>{firstName} {lastName}</span>
+    )
+  }
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <Greeting /> <Name firstName="Katya" lastName="Bindyuk"/>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 
