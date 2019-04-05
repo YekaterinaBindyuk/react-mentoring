b9d0a2db0f49e25f23f73cfb48cc1ad4d9f5da3c
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-05 20:35:07 +0600

    test

---
+++ movies/src/components/search-results/SearchResultsCount.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, mount } from "enzyme";
+import { MOVIES_COUNT_TEXT } from "../../environment/const";
+
+import SearchResultsCount from "./SearchResultsCount";
+describe("SearchResults", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = render(<SearchResultsCount debug count={1} />);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+it("renders movies count", () => {
+  const count = 1;
+  const wrapper = mount(<SearchResultsCount count={count} />);
+  expect(wrapper.html()).toEqual(
+    '<div class="movies-count"><div class="badge badge-dark p-2"><i class="fa"></i><span>' +
+      MOVIES_COUNT_TEXT +
+      ' <span class="badge badge-light">' +
+      count +
+      "</span></span></div></div>"
+  );
+});
