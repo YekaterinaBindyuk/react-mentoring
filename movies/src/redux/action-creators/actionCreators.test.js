29a85716169a6e80fbc457f6f9ac16195c466df7
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-05 21:52:37 +0600

    action creators test

--- movies/src/redux/action-creators/actionCreators.test.js
+++ movies/src/redux/action-creators/actionCreators.test.js
@@ -1,15 +1,32 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import * as actions from './actionCreators';
-import * as types from '../actions/actions';
-import fetchMock from 'fetch-mock';
-import expect from 'expect'; 
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import * as actions from "./actionCreators";
+import * as types from "../actions/actions";
+import fetchMock from "fetch-mock";
+import expect from "expect";
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-describe('async actions', () => {
+describe("async actions", () => {
   afterEach(() => {
-    fetchMock.restore()
-  })
-})
+    fetchMock.restore();
+  });
+  it("creates MOVIES_LIST_SUCCESS when fetching movies has been done", () => {
+    fetchMock.getOnce("/movies", {
+      body: { movies: ["test"]},
+      headers: { "content-type": "application/json" }
+    });
+
+    const expectedActions = [
+      { type: types.GET_MOVIE_LIST },
+      { type: types.GET_MOVIE_LIST_SUCCESS, body: { movies: ["test"]} }
+    ];
+    const store = mockStore({ movies: [], movie: {}, recommendedMovies: [] });
+
+    return store.dispatch(actions.getMovieList()).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
