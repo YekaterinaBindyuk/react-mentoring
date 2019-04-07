b845083c10036e60d702397c2f12b44d826b8d5c
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-07 13:41:59 +0600

    config store

--- movies/src/redux/reducers/configureStore.js
+++ movies/src/redux/reducers/configureStore.js
@@ -1,18 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
- 
-import rootReducer from './reducers'
+import thunk from "redux-thunk";
+
+import reducer from './reducer'
  
 const persistConfig = {
   key: 'root',
   storage,
 }
  
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, reducer)
  
 export default () => {
-  let store = createStore(persistedReducer)
+  let store = createStore(persistedReducer, applyMiddleware(thunk))
   let persistor = persistStore(store)
   return { store, persistor }
 }
