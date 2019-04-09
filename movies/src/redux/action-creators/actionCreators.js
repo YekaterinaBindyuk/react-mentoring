51d3750fbac3a1104fac6c6307fa38744c42acd3
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-09 20:43:09 +0600

    test

--- movies/src/redux/action-creators/actionCreators.js
+++ movies/src/redux/action-creators/actionCreators.js
@@ -1,60 +1,76 @@
 import services from "../services/services";
-import { GET_MOVIE_LIST_SUCCESS, GET_MOVIE_SUCCESS, GET_RECOMMENDED_MOVIES_SUCCESS, SORT_MOVIES_SUCCESS, SEARCH_MOVIES_SUCCESS } from "../actions/actions";
+import {
+  GET_MOVIE_LIST_SUCCESS,
+  GET_MOVIE_SUCCESS,
+  GET_RECOMMENDED_MOVIES_SUCCESS,
+  SORT_MOVIES_SUCCESS,
+  SEARCH_MOVIES_SUCCESS
+} from "../actions/actions";
 
-const getMovieList = () => dispatch => {
+export function getMovieList() {return dispatch => {
   services.getMovies().then(res => {
     dispatch(getMovieListSuccess(res.data));
   });
-};
+}};
 
-const getMovieListSuccess = movies => ({
-  type: GET_MOVIE_LIST_SUCCESS,
-  movies
-});
+export function getMovieListSuccess(movies) {
+  return {
+    type: GET_MOVIE_LIST_SUCCESS,
+    movies
+  };
+}
 
-const getMovie = (id) => dispatch => {
+export function getMovie(id){return dispatch => {
   services.getMovie(id).then(res => {
     dispatch(getMovieSuccess(res));
   });
-};
+}};
 
-const getMovieSuccess = movie => ({
-  type: GET_MOVIE_SUCCESS,
-  movie
-});
+export function getMovieSuccess(movie) {
+  return {
+    type: GET_MOVIE_SUCCESS,
+    movie
+  };
+}
 
-const getRecommendedMovies  = (genres) => dispatch => {
+const getRecommendedMovies = genres => dispatch => {
   services.getRecommendedMovies(genres).then(res => {
     dispatch(getRecommendedMoviesSuccess(res.data));
-  })
+  });
 };
 
-const getRecommendedMoviesSuccess = movies => ({
-  type: GET_RECOMMENDED_MOVIES_SUCCESS,
-  movies
-});
+export function getRecommendedMoviesSuccess(movies) {
+  return {
+    type: GET_RECOMMENDED_MOVIES_SUCCESS,
+    movies
+  };
+}
 
-const sortMovies  = (sortBy) => dispatch => {
+const sortMovies = sortBy => dispatch => {
   services.sortMovies(sortBy).then(res => {
     dispatch(sortMoviesSuccess(res.data));
-  })
+  });
 };
 
-const sortMoviesSuccess = movies => ({
-  type: SORT_MOVIES_SUCCESS,
-  movies
-});
+export function sortMoviesSuccess(movies) {
+  return {
+    type: SORT_MOVIES_SUCCESS,
+    movies
+  };
+}
 
-const searchMovies  = (searchValue, searchBy) => dispatch => {
+const searchMovies = (searchValue, searchBy) => dispatch => {
   services.searchMovies(searchValue, searchBy).then(res => {
     dispatch(searchMoviesSuccess(res.data));
-  })
+  });
 };
 
-const searchMoviesSuccess = movies => ({
-  type: SEARCH_MOVIES_SUCCESS,
-  movies
-});
+export function searchMoviesSuccess(movies){
+  return{
+    type: SEARCH_MOVIES_SUCCESS,
+    movies
+  }
+};
 
 export default {
   getMovieList,
--- movies/src/redux/action-creators/actionCreators.test.js
+++ movies/src/redux/action-creators/actionCreators.test.js
@@ -4,6 +4,7 @@ import * as actions from "./actionCreators";
 import * as types from "../actions/actions";
 import fetchMock from "fetch-mock";
 import expect from "expect";
+import {getMovieList, getMovieListSuccess, getMovieSuccess, getRecommendedMoviesSuccess, sortMoviesSuccess, searchMoviesSuccess} from "./actionCreators";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -14,18 +15,63 @@ describe("async actions", () => {
   });
   it("creates MOVIES_LIST_SUCCESS when fetching movies has been done", () => {
     fetchMock.getOnce("/movies", {
-      body: { movies: ["test"]},
+      body: { movies: ["test"] },
       headers: { "content-type": "application/json" }
     });
     const expectedActions = [
       { type: types.GET_MOVIE_LIST },
-      { type: types.GET_MOVIE_LIST_SUCCESS, body: { movies: ["test"]} }
+      { type: types.GET_MOVIE_LIST_SUCCESS, body: { movies: ["test"] } }
     ];
     const store = mockStore({ movies: [], movie: {}, recommendedMovies: [] });
 
-    return store.dispatch(actions.getMovieList()).then(() => {
+    return store.dispatch(getMovieList()).then(() => {
       // return of async actions
       expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
+
+describe("Movie list success", () => {
+  it("returnes correct value", () => {
+    expect(getMovieListSuccess(["test"])).toEqual({
+      type: types.GET_MOVIE_LIST_SUCCESS,
+      movies: ["test"]
+    });
+  });
+});
+
+describe("Movie success", () => {
+  it("returnes correct value", () => {
+    expect(getMovieSuccess({name: "test"})).toEqual({
+      type: types.GET_MOVIE_SUCCESS,
+      movie: {name: "test"}
+    });
+  });
+});
+
+describe("Recommended Movies success", () => {
+  it("returnes correct value", () => {
+    expect(getRecommendedMoviesSuccess(["test"])).toEqual({
+      type: types.GET_RECOMMENDED_MOVIES_SUCCESS,
+      movies: ["test"]
+    });
+  });
+});
+
+describe("Sort Movies success", () => {
+  it("returnes correct value", () => {
+    expect(sortMoviesSuccess(["test"])).toEqual({
+      type: types.SORT_MOVIES_SUCCESS,
+      movies: ["test"]
+    });
+  });
+});
+
+describe("Sort Movies success", () => {
+  it("returnes correct value", () => {
+    expect(searchMoviesSuccess(["test"])).toEqual({
+      type: types.SEARCH_MOVIES_SUCCESS,
+      movies: ["test"]
     });
   });
 });
