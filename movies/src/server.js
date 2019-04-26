9f2356aa837cb35ba71f8647706dd7436599c045
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-26 22:57:58 +0600

    fix

---
+++ movies/src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("*", (req, res) => {
+      return handle(req, res, "/index");
+    });
+    server.get("/about", (req, res) => {
+      const actualPage = "/about";
+      app.render(req, res, actualPage);
+    });
+    server.get("/movie", (req, res) => {
+      const actualPage = "/movieView";
+      const queryParams = { id: req.params.id };
+      app.render(req, res, actualPage, queryParams);
+    });
+    server.listen(8081, err => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:8081");
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
