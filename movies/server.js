16b71632b8204d2d41649c25be7281d64f21472e
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-04-26 22:09:46 +0600

    fix

--- movies/server.js
+++ movies/server.js
@@ -1,10 +1,28 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(8081, function () {
-  console.log('App listening on port 8081!');
-});
+const express = require('express')
+const next = require('next')
+    
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+    
+app.prepare()
+.then(() => {
+  const server = express()
+    
+  server.get('*', (req, res) => {
+    return handle(req, res, '/index')
+  })
+  server.get('/index', (req, res) => {
+    const actualPage = '/index'
+    const queryParams = { id: req.params.id } 
+    app.render(req, res, actualPage)
+})   
+  server.listen(8081, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:8081')
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
