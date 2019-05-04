24eca475cf80f6a4f36eed2580fc39b790e30db6
Yekaterina Bindyuk <Yekaterina_Bindyuk@epam.com>
2019-05-04 23:25:57 +0600

    fix

--- movies/src/selectors/sortingSelector.js
+++ movies/src/selectors/sortingSelector.js
@@ -5,11 +5,11 @@ const getMovies = (state) => state.movies;
 const getSortingType = (state) => state.sortingType;
 
 const sortByRatingCallback = (movie1, movie2) => {
-  if(movie1.vote_average <= movie2.vote_average) {return 1} else {return -1}
+  if(movie1.title <= movie2.title) {return 1} else {return -1}
 
 };
 const sortByReleaseDateCallback = (movie1, movie2) => {
-  if(moment(movie1.release_date).isSameOrBefore(moment(movie2.release_date))) {return 1} else {return -1}
+  if(moment(movie1.vote_count).isSameOrBefore(moment(movie2.vote_count))) {return 1} else {return -1}
 };
 
 export const sortMovies = createSelector(
